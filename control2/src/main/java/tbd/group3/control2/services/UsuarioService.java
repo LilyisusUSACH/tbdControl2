2462d220001ba6b3a2c4b23f3b1d61bb419bdf76
Nicolas <nicolas.cartes@usach.cl>
2024-04-29 01:12:14 -0400

    [Nicolas Espina] agregue usuariorepository

--- control2/src/main/java/tbd/group3/control2/services/UsuarioService.java
+++ control2/src/main/java/tbd/group3/control2/services/UsuarioService.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class UsuarioService {
     @Autowired
     JwtUtils jwtUtils;
+    UsuarioRepository usuarioRepository;
 
     public List<UsuarioEntity> getUsuarios(){
         return usuarioRepository.findAll();
